e9c25d4c27b7414b0ccd620b958fbf5a6d772dd8
jinnuT <jinnuaz@hotmail.com>
2021-07-24 20:01:12 -0700

    creating ec2 attribute with a security group

--- 03-example1/ec2.tf
+++ 03-example1/ec2.tf
@@ -1,14 +1,14 @@
 
-                /*
+
 resource "aws_instance" "sample" {
   ami                    = "ami-074df373d6bafa625"
   instance_type          = "t3.micro"
-  vpc_security_group_ids = []
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   tags                   = {
     Name                 = "Sample"
   }
 }
-                */
+
 resource "aws_security_group" "allow_ssh" {
   name                = "allow_ssh"
   description         = "allow_ssh"
@@ -34,10 +34,9 @@ resource "aws_security_group" "allow_ssh" {
   }
 }
 
-output "sg-attributes" {
-  value = aws_security_group.allow_ssh
+output "ec2-attributes" {
+  value = aws_instance.sample
 }
-
 provider "aws" {
   region = "us-east-1"
 }
