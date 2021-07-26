47c1b4eefc9fed198a6e5bede7cfd7801ec121f9
jinnuT <jinnuaz@hotmail.com>
2021-07-26 14:22:32 -0700

    Dynamodb

--- 04-remote-state/sample.tf
+++ 04-remote-state/sample.tf
@@ -1,9 +1,9 @@
 
 resource "aws_instance" "sample" {
-  ami           = "ami-074df373d6bafa625"
-  instance_type = "t3.micro"
-  tags          = {
-    Name        = "Sample"
+  ami               = "ami-074df373d6bafa625"
+  instance_type     = "t3.micro"
+  tags              = {
+    Name            = "Sample"
   }
 }
 
@@ -13,8 +13,9 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket      = "terraform-j57"
-    key         = "sample/terraform.tfstatefile"
-    region      = "us-east-1"
+    bucket          = "terraform-j57"
+    key             = "sample/terraform.tfstatefile"
+    region          = "us-east-1"
+    dynamodb_table  = "terraform"
   }
 }
