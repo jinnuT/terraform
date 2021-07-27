f51e471f3e73a114e8d56e5a8cb58604fb285c65
jinnuT <jinnuaz@hotmail.com>
2021-07-27 13:22:59 -0700

    terraform complete run

--- roboshop-shell-scripting/ec2.tf
+++ roboshop-shell-scripting/ec2.tf
@@ -1,23 +1,41 @@
 
 resource "aws_spot_instance_request" "cheap_worker" {
-  count                           = length(var.COMPONENTS)
+  count                           = local.LENGTH
   ami                             = "ami-074df373d6bafa625"
   spot_price                      = "0.0031"
   instance_type                   = "t3.micro"
+  vpc_security_group_ids          = ["sg-0a3395ef37041658b"]
   wait_for_fulfillment            = true
   tags                            = {
     Name                          = element(var.COMPONENTS, count.index)
   }
 }
 
-//resource "aws_ec2_tag" "name-tag" {
-//  count                           = length(var.COMPONENTS)
-//  resource_id                     = aws_spot_instance_request.cheap_worker.*.spot_instance_id
-//  key                             = "Name"
-//  value                           = element(var.COMPONENTS,count.index)
-//}
+resource "aws_ec2_tag" "name-tag" {
+  count                           = local.LENGTH
+  resource_id                     = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id,count.index)
+  key                             = "Name"
+  value                           = element(var.COMPONENTS,count.index)
+}
 
-output "attributes" {
-  value = aws_spot_instance_request.cheap_worker.*.spot_instance_id
+resource "null_resource" "run-shell-scripting" {
+  count                           = local.LENGTH
+  provisioner "remote-exec" {
+    connection {
+      user                        = "centos"
+      password                    = "DevOps321"
+      host                        = element(aws_spot_instance_request.cheap_worker.*.public_ip,count.index)
+    }
+    command = [
+      "cd /home/centos",
+      "git clone https://github.com/jinnuT/shell_scripting.git",
+      "cd shell-scripting/roboshop",
+      "sudo make ${element(var.COMPONENTS,count.index)}"
+    ]
+  }
+}
+
+locals {
+  LENGTH = length(var.COMPONENTS)
 }
 
