8be14fe7bf0a888c6918e03ff9fb1232429acb47
jinnuT <jinnuaz@hotmail.com>
2021-07-27 11:23:24 -0700

    just checking the output of attributes to see [aws_spot_instance_request.cheap_worker.*.spot_instance_id]

--- roboshop-shell-scripting/ec2.tf
+++ roboshop-shell-scripting/ec2.tf
@@ -9,3 +9,14 @@ resource "aws_spot_instance_request" "cheap_worker" {
     Name                          = element(var.COMPONENTS, count.index)
   }
 }
+
+//resource "aws_ec2_tag" "name-tag" {
+//  count                           = length(var.COMPONENTS)
+//  resource_id                     = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
+//  key                             = "Name"
+//  value                           = element(var.COMPONENTS, count.index )
+//}
+
+output "attributes" {
+  value = aws_spot_instance_request.cheap_worker.*.spot_instance_id
+}
