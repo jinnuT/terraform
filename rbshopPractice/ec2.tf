f497bfa3f3e164d4076bb78fbc9849ec67554f74
jinnuT <jinnuaz@hotmail.com>
2021-07-28 16:08:16 -0700

    wait_for_fulfillment not added

--- rbshopPractice/ec2.tf
+++ rbshopPractice/ec2.tf
@@ -8,6 +8,7 @@ resource "aws_spot_instance_request" "cheap_worker" {
   ami                             = "ami-074df373d6bafa625"
   spot_price                      = "0.0031"
   instance_type                   = "t3.micro"
+  wait_for_fulfillment            = true
   tags                            = {
     Name                          = element(var.COMPONENT,count.index )
   }
