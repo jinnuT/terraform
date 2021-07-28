152b350531c59a352217bc477a4a02675f98af38
jinnuT <jinnuaz@hotmail.com>
2021-07-28 16:57:20 -0700

    frontend activation

--- rbshopPractice/ec2.tf
+++ rbshopPractice/ec2.tf
@@ -8,6 +8,7 @@ resource "aws_spot_instance_request" "cheap_worker" {
   ami                             = "ami-074df373d6bafa625"
   spot_price                      = "0.0031"
   instance_type                   = "t3.micro"
+  vpc_security_group_ids          = ["sg-04a7ddc2077ef1099"]
   wait_for_fulfillment            = true
   tags                            = {
     Name                          = element(var.COMPONENT,count.index )
@@ -19,6 +20,27 @@ resource "aws_ec2_tag" "name-tag" {
   key                             = "Name"
   value                           = element(var.COMPONENT,count.index )
 }
+
+resource "null_resource" "run-shell-script" {
+  count                           = length(var.COMPONENT)
+  provisioner "remote-exec" {
+    connection {
+      type                        = "ssh"
+      user                        = "centos"
+      password                    = "DevOps321"
+      host                        = element(aws_spot_instance_request.cheap_worker.*.public_ip,count.index )
+    }
+
+    inline = [
+      "cd /home/centos",
+      "git clone https://github.com/jinnuT/shell_scripting.git",
+      "cd shell_scripting/roboshop",
+      "sudo make ${element(var.COMPONENT, count.index)}"
+
+    ]
+  }
+}
+
 
 
 
