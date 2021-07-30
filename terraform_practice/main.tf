425d7d49ab2dd9e16c3b2e31bbc92855cd88ba3c
jinnuT <jinnuaz@hotmail.com>
2021-07-30 12:12:06 -0700

    prefix

--- terraform_practice/main.tf
+++ terraform_practice/main.tf
@@ -7,7 +7,7 @@ resource "local_file" "car" {
   content        = var.contentO
 }
 resource "random_pet" "my-pet" {
-  prefix          = var.prefix
+  prefix          = var.prefix[3]
   separator       = var.separator
   length          = var.length
 }
