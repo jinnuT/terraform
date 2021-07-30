4853275ea6c975762311ebed7e35946252a0c7a0
jinnuT <jinnuaz@hotmail.com>
2021-07-30 11:41:53 -0700

    updated files

--- terraform_practice/main.tf
+++ terraform_practice/main.tf
@@ -1,14 +1,14 @@
 resource "local_file" "pet" {
-  filename0 = "${var.filenameZ}"
-  content0 = "${var.contentZ}"
+  filename0       = var.filenameZ
+  content0        = var.contentZ
 }
 resource "local_file" "car" {
-  filename1 = "${var.filenameO}"
-  content2 = "${var.contentO}"
+  filename1       = var.filenameO
+  content2        = var.contentO
 }
 resource "random_pet" "my-pet" {
-  prefix      = var.prefix
-  separator   = var.separator
-  lenght      = var.lenght
+  prefix          = var.prefix
+  separator       = var.separator
+  lenght          = var.lenght
 }
 
