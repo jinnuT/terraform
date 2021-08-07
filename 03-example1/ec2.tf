d26d0b5b769576fe2e8634e5a37ea2de94aaab92
jinnuT <jinnuaz@hotmail.com>
2021-08-06 23:24:04 -0700

    revision

--- 03-example1/ec2.tf
+++ 03-example1/ec2.tf
@@ -1,13 +1,13 @@
 
 
-//resource "aws_instance" "sample" {
-//  ami                    = "ami-074df373d6bafa625"
-//  instance_type          = "t3.micro"
-//  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-//  tags                   = {
-//    Name                 = "Sample"
-//  }
-//}
+resource "aws_instance" "sample" {
+  ami                    = "ami-074df373d6bafa625"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  tags                   = {
+    Name                 = "Sample"
+  }
+}
 
 resource "aws_security_group" "allow_ssh" {
   name                = "allow_ssh"
@@ -34,13 +34,15 @@ resource "aws_security_group" "allow_ssh" {
   }
 }
 
-//output "ec2-attributes" {
-//  value = aws_instance.sample
+
+//output "sg_attribute" {
+//  value = aws_security_group.allow_ssh.id
 //}
 
-output "sg_attribute" {
-  value = aws_security_group.allow_ssh.id
+output "ec2-attributes" {
+  value = aws_instance.sample
 }
+
 provider "aws" {
   region = "us-east-1"
 }
