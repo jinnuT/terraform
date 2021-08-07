1c7f7f02df57f7a3f9337574ee3d2c49be605283
jinnuT <jinnuaz@hotmail.com>
2021-08-06 22:49:43 -0700

    revision

--- 03-example1/ec2.tf
+++ 03-example1/ec2.tf
@@ -1,13 +1,13 @@
 
 
-resource "aws_instance" "sample" {
-  ami                    = "ami-074df373d6bafa625"
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-  tags                   = {
-    Name                 = "Sample"
-  }
-}
+//resource "aws_instance" "sample" {
+//  ami                    = "ami-074df373d6bafa625"
+//  instance_type          = "t3.micro"
+//  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+//  tags                   = {
+//    Name                 = "Sample"
+//  }
+//}
 
 resource "aws_security_group" "allow_ssh" {
   name                = "allow_ssh"
